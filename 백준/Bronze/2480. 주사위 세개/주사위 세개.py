2788a9aff69e9f01932393c5621c51bcb01db767
안정인 <140046680+JungInAn@users.noreply.github.com>
2023-09-05 17:11:36 +0900

    [Bronze IV] Title: 주사위 세개, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+x,y,z = map(int, input().split())
+if x==y==z :
+    print(10000+x*1000)
+elif x==y :
+    print(1000+x*100)
+elif x==z :
+    print(1000+x*100)
+elif y==z :
+    print(1000+y*100)
+else :
+    if x>y and x>z :
+        print(x*100)
+    elif y>x and y>z :
+        print(y*100)
+    else :
+        print(z*100)
